3a8e3d4a62109d340153ba1e7b0430a77212c324
unknown <erradja.marwane@gmail.com>
2019-05-05 16:39:16 +0200

    first commit

---
+++ apiGetaway-service/src/main/java/fr/amine/zuul/ApiGetawayServiceApplication.java
@@ -0,0 +1,15 @@
+package fr.amine.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGetawayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGetawayServiceApplication.class, args);
+	}
+
+}
---
+++ produit-service/src/test/java/fr/amine/produit/ProduitServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.amine.produit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProduitServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
